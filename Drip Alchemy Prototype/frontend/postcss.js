09559fda718d8a14e98dda38df0b64a168743a8e
patterson0407 <patterson0407@gmail.com>
2025-04-06 23:35:36 -0700

    Delete Drip Alchemy Prototype directory

--- Drip Alchemy Prototype/frontend/postcss.js
+++
@@ -1,7 +0,0 @@
-// frontend/postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {}, // <--- Must be present
-    autoprefixer: {}, // <--- Standard addition
-  },
-}
--- Drip Alchemy Prototype/src/utils/connectors.js
+++
@@ -1,6 +0,0 @@
-import { InjectedConnector } from '@web3-react/injected-connector';
-
-// Define supported chains visually, even if not strictly enforced in prototype
-const supportedChainIds = [56, 97]; // BSC Mainnet, BSC Testnet
-
-export const injected = new InjectedConnector({ supportedChainIds });
